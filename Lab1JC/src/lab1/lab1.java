a44d7d2def892b611c9327d1007f2aad1f612e82
jadec <jadec@128.4.89.86>
2016-09-09 14:11:04 -0400

    formatted the string

--- Lab1JC/src/lab1/lab1.java
+++ Lab1JC/src/lab1/lab1.java
@@ -10,14 +10,22 @@ public class lab1 {
 		System.out.print("Enter how many years you plan to work: ");
 		int work = answer.nextInt()*12;
 		
+		double returned1;
+		do{
 		System.out.print("Enter expected average return on investment: ");
-		double returned1 = answer.nextDouble()/1200;
+		returned1 = answer.nextDouble();
+		}while (returned1<=0 || returned1>20);
+		returned1=returned1/ 1200;
 		
 		System.out.print("Enter how long you want to draw: ");
 		int retired = answer.nextInt()*12;
 		
+		double returned2;
+		do{
 		System.out.print("Enter expected average return on investment: ");
-		double returned2 = answer.nextDouble()/1200;
+		returned2 = answer.nextDouble();
+		}while (returned2<=0 || returned2>3);
+		returned2=returned2/ 1200;
 		
 		System.out.print("Enter required income: ");
 		double income = answer.nextDouble();
@@ -31,9 +39,9 @@ public class lab1 {
 		
 		double monthly = (future * returned1)/((Math.pow(1+returned1, work))-1);
 		
-		System.out.printf("What you should save every month " + monthly+"\n");
+		System.out.printf("What you should save every month $%.2f\n", monthly);
 		
-		System.out.printf("What you needed to save " + future);
+		System.out.printf("What you needed to save $%.2f", future);
 	}
 	
 }
